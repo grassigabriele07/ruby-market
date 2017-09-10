dfa4830bdef68c8f7c236ce5dded0b7b6870d78a
grassigabriele07 <gabrielegrassi@iMac-di-Gabriele.local>
2017-09-10 22:16:45 +0200

    aggiunta pagina di about

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   #controller pages imposto l'homepage
   root 'pages#home'
 
+  get 'about' => 'pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
